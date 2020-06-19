361c8d4eb5a1908ea3f81cd11fc1baa2872da2c4
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-19 16:10:40 +0700

    istimiwir 2

--- src/main/java/com/fahrul/springjenkinsdocker/SpringJenkinsDockerApplication.java
+++ src/main/java/com/fahrul/springjenkinsdocker/SpringJenkinsDockerApplication.java
@@ -2,9 +2,17 @@ package com.fahrul.springjenkinsdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsDockerApplication {
+	
+	@GetMapping("/")
+	public String ping() {
+		return "Istimiwir kiwir kiwir";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsDockerApplication.class, args);
